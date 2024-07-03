610bc3fb06cd3241355b4731b22f67608a22f10f
Charlie Lambino-Worthington <charlie.m.k.worthington@gmail.com>
2024-07-03 08:23:12 +0000

    add book model and comment model

--- bookingstablestays/models.py
+++ bookingstablestays/models.py
@@ -1,3 +1,59 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
+STATUS = ((0, "unbooked"), (1, "booked"))
 
 # Create your models here.
+
+
+class Book(models.Model):
+    """
+    Stores a single booking entry related to :model:`auth.User`.
+    """
+    horse_name = models.CharField(max_length=200, unique=True)
+    bookingid = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="book_posts"
+    )
+    feeding_requirements = models.TextField()
+    exercise_requirements = models.TextField()
+    staystart = models.DateField()
+    stayend = models.DateField()
+    number_nights =
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"{self.horse_name} | written by {self.author}"
+
+
+class Comment(models.Model):
+    """
+    Stores a single comment entry related to :model:`auth.User`
+    and :model:`blog.Post`.
+    """
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name="comments")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter")
+       def average_rating(self) -> float:
+        return Rating.objects.filter(post=self).aggregate(Avg("rating"))["rating__avg"] or 0
+
+    def __str__(self):
+        return f"{self.header}: {self.average_rating()}"
+   
+    body = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholder')
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
