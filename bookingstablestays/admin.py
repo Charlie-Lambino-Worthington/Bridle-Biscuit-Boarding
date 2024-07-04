11a025d67f6a9bc9e0f49cbe71a8e140adda5fe7
Charlie Lambino-Worthington <charlie.m.k.worthington@gmail.com>
2024-07-04 14:07:21 +0000

    update admin.py

--- bookingstablestays/admin.py
+++ bookingstablestays/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Book, stables, stable_availability, Review
+from django_summernote.admin import SummernoteModelAdmin
+
+@admin.register(Book)
+class PostAdmin(SummernoteModelAdmin):
+
+    list_display = ('horse_name', 'status', 'created_on')
+    search_fields = ['horse_name', 'bookingid']
+    list_filter = ('status', 'created_on',)
+    prepopulated_fields = {'bookingid': ('horse_name','created_on')}
+    summernote_fields = ('feeding_requirements', 'exercise_requirements')
+
 
 # Register your models here.
+admin.site.register(Review, stables, stable_availability )
