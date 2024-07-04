bc5726798f8306d7ab3f84acd80b3810c7dfc937
Charlie Lambino-Worthington <charlie.m.k.worthington@gmail.com>
2024-07-04 12:21:55 +0000

    add mixin for availability check

--- bookingstablestays/views.py
+++ bookingstablestays/views.py
@@ -8,6 +8,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
 from .models import book, review
 from .forms import BookForm, ReviewForm
+from django.db.models import Q
 
 from django.urls import reverse
 from django.http import HttpResponseRedirect
@@ -15,6 +16,12 @@ from .models import Post
 from .forms import PostForm
 
 # Create your views here.
+class homepage():
+    return "static/index.html"
+
+class facilities():
+    return "static/facilities.html"
+
 
 class Reviewlist(LoginRequiredMixin, generic.ListView):
     template_name = "static/reviews.html"
@@ -39,7 +46,37 @@ class Reviewlist(LoginRequiredMixin, generic.ListView):
             messages.add_message(request, messages.ERROR, 'Error creating review.')
             return self.get(request, *args, **kwargs)
 
-class Booking(LoginRequiredMixin):
+class CheckAvailabilityMixin:
+    def check_stable_availability(self, stable, stay_start, stay_end, current_booking=0):
+        # Convert dates to datetime objects for comparison
+        now = timezone.now().date()
+        stay_start = timezone.make_aware(stay_start).date()
+        stay_end = timezone.make_aware(stay_end).date()
+
+        # Check if stay_start is greater than or equal to today
+        if stay_start < now:
+            messages.error(self.request, "Check-in date is not valid. Please enter a valid date.")
+            return False
+
+        # Check if stay_end is less than or equal to stay_start
+        if stay_end <= stay_start:
+            messages.error(self.request, "Check-out date is not valid. Please enter a valid date.")
+            return False
+
+        # Query to find overlapping bookings
+        overlapping_bookings = Book.objects.filter(
+            Q(stay_start__lte=stay_end, stay_end__gte=stay_start) |
+            Q(stay_start__range=(stay_start, stay_end))
+        ).exclude(id=current_booking)
+
+        if overlapping_bookings.exists():
+            messages.error(self.request, "The selected dates are already booked.")
+            return False
+
+        return True
+
+
+class Booking(LoginRequiredMixin, CheckAvailabilityMixin):
     template_name = "static/book.html"
     def book(self, request, *args, **kwargs):
         form = BookForm(request.Book)
